bf5d3e1ab6191e9ec7dcc4783bef04366abd7b0e
heba-Abdallah <heba.essam.m@hotmail.com>
2021-03-30 10:57:38 +0300

    solve my mistake

--- js/app.js
+++ js/app.js
@@ -5,52 +5,66 @@ alert("HI THERE");
 let yourName = prompt('what\'s your name?')
 alert("wlecome " + yourName);
 
-let myName = prompt('Is My Name HebaEssam?');
-myName = myName.toUpperCase();
+let name = prompt('Is My Name HebaEssam?');
+name = name.toUpperCase();
 
-if (myName === 'YES') {
+if (name === 'YES' || name === 'Y') {
     // console.log(myName);
-    alert(myName);
-} else {
+    alert(name);
+} else if (name === 'NO' || name === 'N') {
     // console.log('No My name is Heba Essam');
     alert('No My name is Heba Essam');
+} else {
+    ('please answer with only yes/y or no/n');
 }
 
-let myAge = prompt('Is My Age 28?');
-myAge = myAge.toUpperCase();
-if (myAge === 'YES') {
+let age = prompt('Is My Age 28?');
+age = age.toUpperCase();
+if (age === 'YES' || age === 'Y') {
     // console.log(myAge);
-    alert(myName);
-} else {
-    // console.log('No Me Age is 28');
+    alert(name);
+} else if (age === 'NO' || age === 'N') {
+     // console.log('No Me Age is 28');
     alert('No Me Age is 28');
-}
-let myNationality = prompt('Do I Have A Jordanian Nationality?');
-myNationality = myNationality.toUpperCase();
-if (myNationality === "YES") {
-    // console.log(myNationality);
-    alert(myNationality);
 } else {
+    ('please answer with only yes/y or no/n');
+} 
+   
+let nationality = prompt('Do I Have A Jordanian Nationality?');
+nationality = nationality.toUpperCase();
+if (nationality === 'YES' || nationality === 'Y') {
+    // console.log(myNationality);
+    alert(nationality);
+} else if (nationality === 'NO' || nationality === 'N') {
     // console.log('My Nationality Is Jordanian');
     alert('My Nationality Is Jordanian');
+
+} else {
+    ('please answer with only yes/y or no/n');
 }
-let myHobbies = prompt('Is My Hobby Reading?');
-myHobbies = myHobbies.toLowerCase();
-if (myHobbies === 'yes') {
+    
+let hobbies = prompt('Is My Hobby Reading?');
+hobbies = hobbies.toLowerCase();
+if (hobbies === 'yes' || hobbies === 'y') {
     // console.log(myHobbies);
     alert(myHobbies);
-} else {
+} else if (hobbies === 'no' || hobbies === 'n') {
     // console.log('My Hobbies Is Reading');
     alert('My Hobbies Is Reading');
+} else {
+    ('please answer with only yes/y or no/n');
 }
-let myStudy = prompt('Did I Study Communication Engineering in BAU?');
-myStudy = myStudy.toLowerCase();
-if (myStudy === 'yes') {
+
+let study = prompt('Did I Study Communication Engineering in BAU?');
+study = study.toLowerCase();
+if (study === 'yes' || study === 'y') {
     // console.log(myStudy);
     alert(myStudy);
-} else {
+} else if (study === 'no' || study === 'n') {
     // console.log('My Study Major Is Communication Engineering');
     alert('My Hobbies Is Reading');
-}
+} else {
+    ('please answer with only yes/y or no/n')
+} 
 
 alert("Thank You For Visiting My Web Page " + yourName);
